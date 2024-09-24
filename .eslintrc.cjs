b63015092777a801dafa8bc5710d74b71b19d18f
xgg0213 <m.eringan@gmail.com>
2024-09-23 17:19:51 -0700

    completed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', 'node_modules'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
@@ -16,5 +17,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off' // <-- Add this line
   },
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+// vite.config.js
+
+import eslint from 'vite-plugin-eslint';
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => ({ // <-- Change this line too!
+  plugins: [
+    react(),
+    eslint({
+      lintOnStart: true,
+      failOnError: mode === "production"
+    })
+  ],
+  server: {
+    open: true
+  }
+}))
